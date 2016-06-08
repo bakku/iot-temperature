76b4cc81197e1858f93ee9e33f15bbb26623938d
Christian Paling <christian.paling@googlemail.com>
2016-06-08 18:28:39 +0200

    vertically aligned content and implemented redirect for root path

--- app/app.rb
+++ app/app.rb
@@ -4,7 +4,9 @@ require 'sinatra/namespace'
 class IotTemperature < Sinatra::Application
 	set :server, :thin
 
+  get '/' do
+    redirect to '/temperature'
+  end
+
   require_relative 'routes/temperature'
 end
-
-

4b40f5248b1c0b4ef87c85f1f58f66cf34f19e0e
Christian Paling <christian.paling@googlemail.com>
2016-06-08 22:11:33 +0200

    implemented data receiver

--- app/lib/data_retriever.rb
+++ app/lib/data_retriever.rb
@@ -1,14 +1,24 @@
+require 'open3'
+
 class DataRetriever
+
+  attr_accessor :humidity, :temperature
+
+  DHT_VERSION = 11
+  GPIO = 4
+
   def initialize
-    output = `sudo python adafruit_retriever.py`
-    puts output
+    stdout, stderr, status = Open3.capture3("sudo python #{File.dirname(__FILE__)}/adafruit_retriever.py #{DHT_VERSION} #{GPIO}")
+
+    if status.success?
+      @humidity = stdout.split("\n").first
+      @temperature = stdout.split("\n").last
+    else
+      @humidity = 'n/a'
+      @temperature = 'n/a'
+    end
   end
 
-  def humidity
-    50
-  end
-
-  def temperature
-    23
-  end
 end
+
+
